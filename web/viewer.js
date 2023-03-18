908feb4da3fedc1291d072be97c7c4b0ab5180ab
Erik D. (@erikd256) <62072464+erikd256@users.noreply.github.com>
2023-03-18 10:40:05 +0200

    update validateFileURL

--- web/viewer.js
+++ web/viewer.js
@@ -1549,7 +1549,7 @@ const PDFViewerApplication = {
 };
 exports.PDFViewerApplication = PDFViewerApplication;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+  const HOSTED_VIEWER_ORIGINS = ["null", "https://viewer.stfranciscus-heverlee.org", "https://res.cloudinary.com"];
   var validateFileURL = function (file) {
     if (!file) {
       return;
